d16a84f81c1ef5672eb8f8fa1a67d399a160404c
SALeon <developleon@gmail.com>
2019-02-02 00:27:39 +0300

    add weback

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      //   {
+      //   enforce: 'pre',
+      //   test: /\.js$/,
+      //   exclude: 'node-modules',
+      //   loader: 'eslint-loader',
+      // },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
